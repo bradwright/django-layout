cbe565506109800f683c0bcd63a47b7d0e45e742
Bradley Wright <brad@intranation.com>
2009-08-29 09:40:34 +0100

    Add missing comma. (Fixes gh-2)

--- yoursite/settings.py
+++ yoursite/settings.py
@@ -70,7 +70,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'django.core.context_processors.auth',
     'django.core.context_processors.debug',
     'django.core.context_processors.i18n',
-    'django.core.context_processors.media'
+    'django.core.context_processors.media',
     'django.core.context_processors.request', #populate the context with the contents of request
 )
 
